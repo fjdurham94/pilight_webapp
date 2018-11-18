834b8c6069915fa6fb7ce1dbdf181bd42fba779a
F J Durham <francisjdurham@gmail.com>
2018-11-18 17:44:16 +0000

    fixed requessts

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,32 @@ import request from 'request';
 
 export default class App extends Component {
     clickedOn() {
-        console.log('Switching lights on')
-        request.post('http://localhost:8081/lights/on')
+        console.log('Switching lights on');
+        request.post('http://localhost:8081/lights/on', (res) => {
+            if (res.statusCode == 200) {
+                console.log('success');
+            } else {
+                console.log('error: ', res);
+            }
+        });
     }
 
     clickedOff() {
         console.log('Switching lights off');
-        request.post('http://localhost:8081/lights/off');
+        request.post('http://localhost:8081/lights/off', (res, err) => {
+            if (res.statusCode == 200) {
+                console.log('success');
+            } else {
+                console.log('error: ', res);
+            }
+        });
     }
 
     render () {
         return <div>
             <p>Light it up</p>
-            <button onClick={ this.clickedOn() }>On</button>
-            <button onClick={ this.clickedOff() }>Off</button>
+            <button onClick={ this.clickedOn }>On</button>
+            <button onClick={ this.clickedOff }>Off</button>
             </div>
     }
 }
