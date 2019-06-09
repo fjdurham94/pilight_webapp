dea2c8236a8613bd70cd31c46b2b3169d65ea969
F J Durham <francisjdurham@gmail.com>
2019-06-09 10:27:32 +0100

    Webpack symantics

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -11,34 +11,45 @@ module.exports = {
         net: 'empty',
         tls: 'empty'
     },
-    entry: [
-        path.join(parentDir, 'src/index.js')
-    ],
+    entry: {
+        app: path.join(parentDir, 'src/index.js')
+    },
     module: {
         rules: [{
             test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader'
-		},{
-			test: /\.css$/,
-			use: {
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: {
                 loader: 'css-loader',
                 options: {
                     sourceMap: true
                 }
             }
-		}],
+        }],
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      }),
-      new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': 'production',
-        }
-      }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"',
+            }
+        }),
     ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
     output: {
         path: parentDir + 'dist',
         filename: 'bundle.js'
