e7a97291f9f71f6d7f4687be1fc909dc9f423d5b
F J Durham <francisjdurham@gmail.com>
2019-06-09 10:08:56 +0100

    server env variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
+const HOST = process.env.HOST_NAME || 'localhost';
+const PORT = process.env.PORT || 8080;
+
 const config = {
-    url: 'http://localhost:8081',
-    port: 8080,
+    url: `http://${HOST}:${PORT}`,
+    port: PORT,
 };
 
 export default config;
