1a98e0e6f312926d5e47ab4d7c1ad82d8fc069f3
F J Durham <francisjdurham@gmail.com>
2019-08-26 16:29:43 +0100

    Add MUI theme provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme } from "@material-ui/core";
 
 import App from './App';
+import { ThemeProvider } from '@material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#228BE6",
+        },
+    }
+});
+
+ReactDOM.render(<ThemeProvider theme={ theme }><App /></ThemeProvider>, document.getElementById('app'))
